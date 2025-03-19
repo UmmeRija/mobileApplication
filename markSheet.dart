06f9236c941e3cf55b917a28da4af1ce83afaf17
UmmeRija <ummerijababar@gmail.com>
2025-03-19 17:06:38 +0500

    operators start

---
+++ markSheet.dart
@@ -0,0 +1,15 @@
+void main(){
+  const total = 500;
+  const math = 90;
+  const english = 85;
+  const physics = 96;
+  const chemistry = 80;
+  const urdu = 82;
+
+  const obtainedMarks = math + english + physics + chemistry + urdu;
+  print(obtainedMarks);
+
+  const result = (obtainedMarks/total)*100;
+  print(result);
+
+}
---
+++ operator.dart
@@ -0,0 +1,7 @@
+void main(){
+  const fistNum = 1;
+  const lastNum = 50;
+  for (var i=0; i<=lastNum; i++){
+    print(i % 2 != 0);
+  }
+}
