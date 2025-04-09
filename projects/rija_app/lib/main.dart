d47c0781aa6d4fdf2dba5b8105ad560526d2f49d
UmmeRija <ummerijababar@gmail.com>
2025-04-09 20:28:41 +0500

    sasa

---
+++ projects/rija_app/lib/main.dart
@@ -0,0 +1,20 @@
+void main (){
+//   Books classFive = Books();
+//   print(classFive.name = "science");
+//   print(classFive.quantity = 100000);
+//   print(classFive.standard = "5th");
+
+//    Books classSix = Books();
+//   print(classSix.name = "Math");
+//   print(classSix.quantity = 200000);
+//   print(classSix.standard = "6th"); 
+// }
+// class Books{
+//   String? name;
+//   int? quantity;
+//   String? standard;
+}
+
+class BookingWork{
+String UserInput =
+}
